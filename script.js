1a53e36c0724254ab290f3ccf17bea7338f0bcdc
FikaKarin <mailtolundqvist@gmail.com>
2023-10-05 18:00:22 +0200

    Add dropdown for description. Add missing imnages for books.

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ const books = [
     rating: 4,
     description:
       'Narrated by the teenage Holden Caulfield, the novel explores themes of alienation and the search for authenticity.',
-    image: './books-images/unknown.jpg',
+    image: './books-images/the-catcher-in-the-rye.jpeg',
   },
   {
     title: 'The Hobbit',
@@ -71,7 +71,7 @@ const books = [
     rating: 4.7,
     description:
       'The first book in the beloved Harry Potter series, it introduces readers to the magical world of Hogwarts and the young wizard Harry Potter.',
-    image: "./books-images/harry-potter-and-the-sorcerer.jpg",
+    image: './books-images/harry-potter-and-the-sorcerer.jpg',
   },
   {
     title: 'Moby-Dick',
@@ -101,7 +101,7 @@ const books = [
     rating: 4.3,
     description:
       "A psychological horror novel that tells the story of the Torrance family's terrifying experiences at the haunted Overlook Hotel.",
-    image: './books-images/unknown.jpg',
+    image: './books-images/the-shining.jpeg',
   },
   {
     title: 'The Chronicles of Narnia: The Lion, the Witch and the Wardrobe',
@@ -121,7 +121,7 @@ const books = [
     rating: 3.8,
     description:
       'A gripping mystery thriller that follows Harvard symbologist Robert Langdon as he unravels the secrets of the Da Vinci Code.',
-    image: './books-images/unknown.jpg',
+    image: './books-images/the-davinci-code.jpeg',
   },
   {
     title: 'The Alchemist',
@@ -131,7 +131,7 @@ const books = [
     rating: 4.25,
     description:
       'A philosophical novel that tells the story of Santiago, a shepherd boy, on his quest to discover his personal legend.',
-    image: './books-images/unknown.jpg',
+    image: './books-images/the-alchemist.jpeg',
   },
   {
     title: 'The Hunger Games',
@@ -141,7 +141,7 @@ const books = [
     rating: 4.3,
     description:
       "In a dystopian future, Katniss Everdeen becomes a symbol of rebellion when she volunteers to take her sister's place in the brutal Hunger Games.",
-    image: './books-images/unknown.jpg',
+    image: './books-images/the-hunger-games.jpeg',
   },
   {
     title: 'The Girl with the Dragon Tattoo',
@@ -151,7 +151,7 @@ const books = [
     rating: 4.1,
     description:
       'A gripping mystery novel featuring investigative journalist Mikael Blomkvist and the enigmatic hacker Lisbeth Salander.',
-    image: './books-images/unknown.jpg',
+    image: './books-images/the-girl-with-the-dragon-tattoo.jpg',
   },
   {
     title: 'The Road',
@@ -161,7 +161,7 @@ const books = [
     rating: 4,
     description:
       "Set in a post-apocalyptic world, it follows a father and son's harrowing journey to survive and find safety.",
-    image: './books-images/unknown.jpg',
+    image: './books-images/the-road.jpg',
   },
   {
     title: "The Hitchhiker's Guide to the Galaxy",
@@ -171,7 +171,7 @@ const books = [
     rating: 4.35,
     description:
       "A comedic science fiction series that follows the misadventures of Arthur Dent after Earth's destruction.",
-    image: './books-images/unknown.jpg',
+    image: './books-images/the-hitchhikers-guide-to-the-galaxy.jpg',
   },
   {
     title: 'The Giver',
@@ -181,7 +181,7 @@ const books = [
     rating: 4.12,
     description:
       'A dystopian novel set in a seemingly perfect society where young Jonas discovers the dark truth beneath the surface.',
-    image: './books-images/unknown.jpg',
+    image: './books-images/the-giver.jpg',
   },
 ];
 
@@ -192,46 +192,85 @@ function filterBooksByGenre(booksArray, genre) {
 
 // Function to display books
 function displayBooks(booksArray) {
-  const bookListContainer = document.getElementById("bookList");
+  const bookListContainer = document.getElementById('bookList');
 
   // Clear any existing content in the container
-  bookListContainer.innerHTML = "";
+  bookListContainer.innerHTML = '';
 
   if (booksArray.length === 0) {
     // Display a message when no books match the filter
-    const noBooksMessage = document.createElement("p");
-    noBooksMessage.textContent = "No books match the selected filter.";
+    const noBooksMessage = document.createElement('p');
+    noBooksMessage.textContent = 'No books match the selected filter.';
     bookListContainer.appendChild(noBooksMessage);
   } else {
     booksArray.forEach((book) => {
       // Create a div element to hold each book's information
-      const bookDiv = document.createElement("div");
-      bookDiv.classList.add("book-item"); // You can style this class in your CSS
+      const bookDiv = document.createElement('div');
+      bookDiv.classList.add('book-item'); // You can style this class in your CSS
 
       // Create a div element to hold the book image
-      const imgDiv = document.createElement("div");
-      imgDiv.classList.add("book-image");
+      const imgDiv = document.createElement('div');
+      imgDiv.classList.add('book-image');
 
       // Create HTML elements for each book property
-      const titleElement = document.createElement("h2");
+      const titleElement = document.createElement('h2');
       titleElement.textContent = book.title;
 
-      const authorElement = document.createElement("p");
+      const authorElement = document.createElement('p');
       authorElement.textContent = `Author: ${book.author}`;
 
-      const yearElement = document.createElement("p");
+      const yearElement = document.createElement('p');
       yearElement.textContent = `Year: ${book.year}`;
 
-      const genreElement = document.createElement("p");
+      const genreElement = document.createElement('p');
       genreElement.textContent = `Genre: ${book.genre}`;
 
-      const ratingElement = document.createElement("p");
+      const ratingElement = document.createElement('p');
       ratingElement.textContent = `Rating: ${book.rating}`;
 
-      const descriptionElement = document.createElement("p");
-      descriptionElement.textContent = book.description;
+      // Create a button for the description dropdown
+      const descriptionButton = document.createElement('button');
+      descriptionButton.classList.add('descriptionButton');
+      descriptionButton.textContent = 'About the book';
 
-      const imageElement = document.createElement("img");
+      // Create arrow elements for indicating dropdown state
+      const arrowDown = document.createElement('span');
+      arrowDown.textContent = '▼'; // Unicode arrow down symbol
+      arrowDown.classList.add('arrow-down');
+
+      const arrowUp = document.createElement('span');
+      arrowUp.textContent = '▲'; // Unicode arrow up symbol
+      arrowUp.classList.add('arrow-up');
+      arrowUp.style.display = 'none'; // Initially hide the arrow up
+
+      // Append arrow elements to the description button
+      descriptionButton.appendChild(arrowDown);
+      descriptionButton.appendChild(arrowUp);
+
+      // Create the description element
+      const descriptionElement = document.createElement('p');
+      descriptionElement.classList.add('descriptionElement'); // Add the class here
+      descriptionElement.textContent = book.description;
+      descriptionElement.style.maxHeight = '0'; // Initially hide the description
+
+      // Add click event listener to toggle description visibility and apply different transition durations
+      descriptionButton.addEventListener('click', () => {
+        const maxHeight = descriptionElement.style.maxHeight;
+        if (maxHeight === '0px' || maxHeight === '') {
+          // If description is hidden or maxHeight is not set (initial state), show it
+          descriptionElement.style.maxHeight = '200px'; // Set the value according to your content height
+          arrowDown.style.display = 'none';
+          arrowUp.style.display = 'inline'; // Show the arrow up when description is expanded
+        } else {
+          // If description is visible, hide it with faster transition
+          descriptionElement.style.maxHeight = '0px';
+          descriptionElement.classList.add('closing'); // Add the closing class
+          arrowDown.style.display = 'inline'; // Show the arrow down when description is collapsed
+          arrowUp.style.display = 'none';
+        }
+      });
+
+      const imageElement = document.createElement('img');
       imageElement.src = book.image;
       imageElement.alt = book.title;
 
@@ -245,6 +284,7 @@ function displayBooks(booksArray) {
       bookDiv.appendChild(yearElement);
       bookDiv.appendChild(genreElement);
       bookDiv.appendChild(ratingElement);
+      bookDiv.appendChild(descriptionButton);
       bookDiv.appendChild(descriptionElement);
 
       // Append the bookDiv to the container
@@ -270,24 +310,24 @@ function sortByTitleDescending() {
 }
 
 // Add event listeners to the sorting buttons
-const sortAscendingButton = document.querySelector("#sortAscendingButton");
-const sortDescendingButton = document.querySelector("#sortDescendingButton");
+const sortAscendingButton = document.querySelector('#sortAscendingButton');
+const sortDescendingButton = document.querySelector('#sortDescendingButton');
 
-sortAscendingButton.addEventListener("click", () => {
+sortAscendingButton.addEventListener('click', () => {
   updateAndDisplaySortedBooks(sortByTitleAscending);
 });
 
-sortDescendingButton.addEventListener("click", () => {
+sortDescendingButton.addEventListener('click', () => {
   updateAndDisplaySortedBooks(sortByTitleDescending);
 });
 
 // Add an event listener to the genre filter select element
-const genreFilterSelect = document.getElementById("genreFilter");
+const genreFilterSelect = document.getElementById('genreFilter');
 
-genreFilterSelect.addEventListener("change", () => {
+genreFilterSelect.addEventListener('change', () => {
   const selectedGenre = genreFilterSelect.value;
 
-  if (selectedGenre === "All") {
+  if (selectedGenre === 'All') {
     // Display all books when "All Genres" is selected
     displayBooks(books);
   } else {
